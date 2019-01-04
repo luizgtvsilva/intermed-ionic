c192d9756a31019db855a5d0e2451327c676d22e
Luiz Gustavo <luiz.gustavo.silva1@outlook.com>
2019-01-03 23:32:57 -0200

    Domain

--- src/main/java/com/luizgtvsilva/simple/resources/CategoriaResources.java
+++ src/main/java/com/luizgtvsilva/simple/resources/CategoriaResources.java
@@ -1,16 +1,30 @@
 package com.luizgtvsilva.simple.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.luizgtvsilva.simple.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResources {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Hello World";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria (1, "Informática");
+		Categoria cat2 = new Categoria (2, "Escritório");
+				
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
+		
 	}
 	
 }
