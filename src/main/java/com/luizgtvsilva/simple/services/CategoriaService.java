8863141faaf42edcd50c35ee446fce91a4ed9e63
Luiz Gustavo <luiz.gustavo.silva1@outlook.com>
2019-01-06 19:38:26 -0200

    Criando repository e service para Categoria

---
+++ src/main/java/com/luizgtvsilva/simple/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.luizgtvsilva.simple.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.luizgtvsilva.simple.domain.Categoria;
+import com.luizgtvsilva.simple.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+		}
+	
+}
