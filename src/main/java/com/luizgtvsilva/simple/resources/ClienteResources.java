e911baeb7cc2357cf51ff15bbd7045ebbbfc767a
Luiz Gustavo <luiz.gustavo.silva1@outlook.com>
2019-01-08 16:45:45 -0200

    Atualizando Categoria com PUT

--- src/main/java/com/luizgtvsilva/simple/resources/ClienteResources.java
+++ src/main/java/com/luizgtvsilva/simple/resources/ClienteResources.java
@@ -20,7 +20,7 @@ public class ClienteResources {
 	
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id) {
+	public ResponseEntity<Cliente> find(@PathVariable Integer id) {
 		
 		Cliente obj = service.find(id);
 		
