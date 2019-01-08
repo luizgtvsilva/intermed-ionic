ddb08f9c61b004531c9848cdf06abe1ec069ddda
Luiz Gustavo <luiz.gustavo.silva1@outlook.com>
2019-01-08 17:55:51 -0200

    Listando todas as Categorias com DTO

--- src/main/java/com/luizgtvsilva/simple/resources/CategoriaResources.java
+++ src/main/java/com/luizgtvsilva/simple/resources/CategoriaResources.java
@@ -1,6 +1,8 @@
 package com.luizgtvsilva.simple.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.luizgtvsilva.simple.domain.Categoria;
+import com.luizgtvsilva.simple.dto.CategoriaDTO;
 import com.luizgtvsilva.simple.services.CategoriaService;
 
 @RestController
@@ -50,6 +53,14 @@ public class CategoriaResources {
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+			
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 			
 	}
 	
--- src/main/java/com/luizgtvsilva/simple/services/CategoriaService.java
+++ src/main/java/com/luizgtvsilva/simple/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.luizgtvsilva.simple.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,5 +43,10 @@ public class CategoriaService {
 		catch (DataIntegrityViolationException e){
 			throw new DataIntegrityException("Não é possível excluir uma Categoria que possui Produtos");
 		}
+		
+	}
+	
+	public List<Categoria> findAll() {
+		return repo.findAll();
 	}
 }
