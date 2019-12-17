170c44df0fd2c4ee17366a3a9c6298cc24410e75
luizgtvsilva <Pessoal@TERC-HDYPOX2.Sulamerica.br>
2019-12-17 20:47:30 -0300

    Validacoes customizadas

--- src/main/java/com/luizgtvsilva/simple/dto/ClienteNewDTO.java
+++ src/main/java/com/luizgtvsilva/simple/dto/ClienteNewDTO.java
@@ -2,20 +2,41 @@ package com.luizgtvsilva.simple.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.luizgtvsilva.simple.services.validation.ClienteInsert;
+
+@ClienteInsert
 public class ClienteNewDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Length(min=3, max=80, message="O tamanho deve ser entre 3 e 80 caracteres")
 	private String nome;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Length(min=5, max=80, message="O tamanho deve ser entre 5 e 80 caracteres")
 	private String email;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String cpfOuCnpj;
 	private Integer tipo;
 	
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Length(min=5, max=100, message="O tamanho deve ser entre 5 e 100 caracteres")
 	private String logradouro;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String numero;
 	private String complemento;
 	private String bairro;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String cep;
 	
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String telefone1;
 	private String telefone2;
 	private String telefone3;
