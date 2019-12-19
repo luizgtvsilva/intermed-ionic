64af985ab3e10abe23bb35634fff71bb39d10408
luizgtvsilva <Pessoal@TERC-HDYPOX2.Sulamerica.br>
2019-12-19 19:40:29 -0300

    Validcao email ja existente

--- src/main/java/com/luizgtvsilva/simple/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/luizgtvsilva/simple/services/validation/ClienteInsertValidator.java
@@ -6,14 +6,21 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.luizgtvsilva.simple.domain.Cliente;
 import com.luizgtvsilva.simple.domain.enums.TipoCliente;
 import com.luizgtvsilva.simple.dto.ClienteNewDTO;
+import com.luizgtvsilva.simple.repositories.ClienteRepository;
 import com.luizgtvsilva.simple.resources.exception.FieldMessage;
 import com.luizgtvsilva.simple.services.validation.utils.BR;
 
 
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	@Autowired
+	ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -29,6 +36,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage ("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já existe!"));
 		}
 
 		for (FieldMessage e : list) {
