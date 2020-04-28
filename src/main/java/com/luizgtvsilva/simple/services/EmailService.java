39b23171d58db86608372bd437f7e3fe2720f40a
luizgtvsilva <Pessoal@TERC-HDYPOX2.Sulamerica.br>
2020-04-28 18:48:08 -0300

    Template de HTML para enviar emails

--- src/main/java/com/luizgtvsilva/simple/services/EmailService.java
+++ src/main/java/com/luizgtvsilva/simple/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.luizgtvsilva.simple.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
 
@@ -11,4 +13,8 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
 }
--- src/main/java/com/luizgtvsilva/simple/services/SmtpEmailService.java
+++ src/main/java/com/luizgtvsilva/simple/services/SmtpEmailService.java
@@ -1,10 +1,13 @@
 package com.luizgtvsilva.simple.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,10 +18,21 @@ public class SmtpEmailService extends AbstractEmailService {
 	@Autowired
 	private MailSender mailSender;
 	
+	@Autowired
+	private JavaMailSender javaMailSender;
+	
 	@Override
 	public void sendEmail(SimpleMailMessage msg) {
 		LOG.info("Enviando pedido de email...");
 		mailSender.send(msg);
+		LOG.info("Email enviado!");
+		
+	}
+
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		LOG.info("Enviando pedido de email HTML...");
+		javaMailSender.send(msg);
 		LOG.info("Email enviado!");
 		
 	}
